9b8f9396bae4f51cd03a9090f341cad22cadc07b
David Rodríguez Miranda <davidrmi@ext.inditex.com>
2025-03-24 10:43:27 +0100

    Initialize Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
     PokemonModule,
   ],
   controllers: [],
