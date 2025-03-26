e480cc13be1727ff10d907b80af51055847caaa0
David Rodríguez Miranda <davidrmi@ext.inditex.com>
2025-03-26 12:20:22 +0100

    implement limit and offset in find all pokemons endpoint

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -9,6 +9,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -38,8 +39,13 @@ export class PokemonService {
     }
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll({ limit, offset }: PaginationDto) {
+    return this.pokemonModel
+      .find()
+      .limit(limit)
+      .skip(offset)
+      .sort({ no: 1 })
+      .select('-__v');
   }
 
   async findOne(term: string) {
